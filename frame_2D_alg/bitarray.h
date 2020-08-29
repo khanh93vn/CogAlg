5e0a20d6f2cd5f29c6348c6dfc42e1c80aa962ea
Khanh Nguyen <khanh93vn@gmail.com>
2020-08-30 00:26:26 +0700

    - Add the compiled Shared Object file - Add params accumulation for frame - Add a hash table to store adjacent pairs using direct hash (Too much memory wasted, needs to be replaced with other hash method) - Comments edits and minor bugfixes

--- frame_2D_alg/bitarray.h
+++ frame_2D_alg/bitarray.h
@@ -10,5 +10,5 @@
 #define setbit(A, i) (A[i >> 5] |= (1 << (i & 0x1F)))
 #define clearbit(A, i) (A[i >> 5] &= ~(1 << (i & 0x1F)))
 #define testbit(A, i) (A[i >> 5] & (1 << (i & 0x1F)))
-#define setbits(A, i) memset(A, 0xFF, (n >> 3) + 1)
-#define clearbits(A, n) memset(A, 0x00, (n >> 3) + 1)
+#define setbits(A, n) memset(A, 0xFF, (n/32 + 1) * sizeof(long))
+#define clearbits(A, n) memset(A, 0x00, (n/32 + 1) * sizeof(long))
